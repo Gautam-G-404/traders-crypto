f87c4bb5754da16a4b784558346c29c5b8014242
Gautam-G-404 <gautam0g6@gmail.com>
2026-08-29 23:11:03 +0530

    UI updated

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <div className="min-h-screen w-full overflow-x-hidden flex flex-col bg-black">
       <Navbar />
-      <main className="grow pb-24 pt-24">
+      <main className="grow pb-12 sm:pb-24 pt-12 sm:pt-24">
         <Toaster position="top-center" reverseOrder={false} />
         <Routes>
           <Route path="/" element={<HomePage />} />
